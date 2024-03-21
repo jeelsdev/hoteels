e9ba734ae25b2ff0b0c402740b837eb8c753539b
Joel Huaman <joelhuaman7m@gmail.com>
2024-03-21 13:05:32 -0500

    [DEV] adding resources and events

--- resources/js/fullcalendar.js
+++ resources/js/fullcalendar.js
@@ -2,6 +2,38 @@ import { Calendar } from 'fullcalendar';
 import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
 
 document.addEventListener('DOMContentLoaded', function() {
+
+  const rooms = [];
+  const events = [];
+
+  if (window.reservations.length > 0)
+  {
+    window.reservations.forEach(event => {
+      events.push({
+        title: event.origin,
+        description: event.origin,
+        start: event.entry_date,
+        end: event.exit_date,
+        color: "#007bff",
+        textColor: "red",
+        resourceId: event.room_id
+      });
+    });
+  }
+
+  if (window.rooms.length > 0)
+  {
+    window.rooms.forEach(room => {
+      rooms.push({
+        id: room.id,
+        building: room.floor,
+        title: `${room.code}-${room.room_type.description}`,
+      });
+    });
+  }
+
+  console.log(events);
+
   const calendarEl = document.getElementById('calendar')
   const calendar = new Calendar(calendarEl, {
     locales: 'es',
@@ -21,24 +53,10 @@ document.addEventListener('DOMContentLoaded', function() {
     aspectRatio: 1.6,
     initialView: 'resourceTimelineDay',
     plugins: [ resourceTimelinePlugin ],
+    dateClick: window.handleDateClick,
     resourceGroupField: 'building',
-    resources: [
-      { id: 'a', building: '460 Bryant', title: 'Auditorium A' },
-      { id: 'b', building: '460 Bryant', title: 'Auditorium B' },
-      { id: 'g', building: '564 Pacific', title: 'Auditorium G' },
-      { id: 'h', building: '564 Pacific', title: 'Auditorium H' },
-      { id: 'i', building: '564 Pacific', title: 'Auditorium I' }
-    ],
-    events: [
-        {
-            title: 'Evento de prueba 1', // le ponemos un título.
-            description: 'Esta es una descripción de prueba', // añadimos la descripción, pero esta solo se muestra al hacer click el evento. 
-            start: '2024-03-10 11:00:00', // definimos cuando comienza para situarlo en el calendario
-            end: '2020-03-10 12:00:00', // y si dura mas de un día podemos definir cuando acaba. Sino lo omitimos
-            color: 'yellow', // color del evento, acepta también hexadecimales.
-            textColor: 'red', // color del texto, acepta igualmente hexadecimales.
-        }
-    ]
+    resources: rooms,
+    events: events
   })
   calendar.render()
 })
