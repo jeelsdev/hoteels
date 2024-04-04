4c79689b3d9cb6cc045e93ff0a71e53907872e3e
Joel Huaman <joelhuaman7m@gmail.com>
2024-04-04 02:36:37 -0500

    [FEAT] updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\ReservationController;
+use App\Livewire\Admin\Dashboard\Dashboard;
+use App\Livewire\Admin\Reservation\EditReservation;
 use App\Livewire\Admin\Reservation\ShowReservations;
 use App\Livewire\Admin\Room\ShowRooms;
+use App\Livewire\Admin\User\Users;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,9 +28,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('admin.dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
 
     Route::prefix('reservation')->group(function () {
         Route::get('/', ShowReservations::class)->name('reservation.index');
@@ -36,4 +37,13 @@ Route::middleware([
     Route::prefix('room')->group(function () {
         Route::get('/', ShowRooms::class)->name('room.index');
     });
+
+    Route::prefix('room')->group(function () {
+        Route::get('/edit', EditReservation::class)->name('edit.index');
+    });
+
+    Route::prefix('users')->group(function () {
+        Route::get('/', Users::class)->name('users.index');
+    });
+    
 });
