c7d9db4da7cdfcb8070cb5bf939b4a6830bc096e
Joel Huaman <joelhuaman7m@gmail.com>
2024-04-04 02:28:44 -0500

    [FEAT] origin enum

---
+++ app/Enums/Origin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Enums;
+
+use ReflectionClass;
+
+enum Origin: string{
+    case Booking = 'booking';
+    case Whatsapp = 'whatsapp';
+    case Llamada = 'llamada';
+    case Facebook = 'facebook';
+    case Calle = 'calle';
+
+    public static function getRandomValue()
+    {
+        $class = new ReflectionClass(self::class);
+        $constants = $class->getConstants();
+        $ramdonKey = array_rand($constants);
+
+        return $constants[$ramdonKey];
+    }
+
+    public static function getArrayValues(): array
+    {
+        $class = new ReflectionClass(self::class);
+        return $class->getConstants();
+    }
+
+}
