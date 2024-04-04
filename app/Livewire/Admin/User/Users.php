e4a79cc97c4730bddd4e1b58c6af6b8aa4d6d6d6
Joel Huaman <joelhuaman7m@gmail.com>
2024-04-04 02:35:30 -0500

    [FEAT] module user created

---
+++ app/Livewire/Admin/User/Users.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire\Admin\User;
+
+use App\Models\User;
+use Livewire\Component;
+
+class Users extends Component
+{
+    public $search = '';
+
+    public function render()
+    {
+        $users = User::where('name', 'like', '%'.$this->search.'%')->orderBy('created_at', 'desc')->paginate(20);
+        return view('livewire.admin.user.users', compact('users'));
+    }
+}
