c1b3843454b9a7dccf4f761eaa3d81056b82d97c
Joel Huaman <joelhuaman7m@gmail.com>
2024-04-11 19:44:51 -0500

    [FIX] components button and input

--- resources/views/components/apps/input.blade.php
+++ resources/views/components/apps/input.blade.php
@@ -1,2 +1,2 @@
 @props(['type' => 'text', 'name' => '', 'id' => '', 'placeholder' => '', 'required' => 'true', 'value'=>''])
-<input type="{{ $type }}" name="{{ $name }}" id="{{ $id }}" placeholder="{{ $placeholder }}" value="{{ $value }}" {{ $attributes->merge(['class'=>'border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5']) }} {{ $required=='true'?'required':'' }}>
+<input type="{{ $type }}" name="{{ $name }}" id="{{ $id }}" placeholder="{{ $placeholder }}" value="{{ $value }}" {{ $attributes->merge(['class'=>'border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-vulcan-50 focus:border-vulcan-300 block w-full p-2.5']) }} {{ $required=='true'?'required':'' }}>
