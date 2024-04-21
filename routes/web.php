3fe792e06d42ff1ddef0f322e00fa9a56c49941b
Joel Huaman <joelhuaman7m@gmail.com>
2024-04-21 10:37:05 -0500

    [FEAT] Add routes for creating and editing rooms, users, tours, and extras

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,15 @@ use App\Http\Controllers\ReservationController;
 use App\Livewire\Admin\Dashboard\Dashboard;
 use App\Livewire\Admin\Reservation\EditReservation;
 use App\Livewire\Admin\Reservation\ShowReservations;
+use App\Livewire\Admin\Room\CreateRoom;
+use App\Livewire\Admin\Room\EditRoom;
 use App\Livewire\Admin\Room\ShowRooms;
+use App\Livewire\Admin\Service\Tour\CreateTour;
+use App\Livewire\Admin\Service\Tour\Tours;
+use App\Livewire\Admin\Service\Xtra\CreateXtra;
+use App\Livewire\Admin\Service\Xtra\Xtras;
+use App\Livewire\Admin\User\CreateUser;
+use App\Livewire\Admin\User\EditUser;
 use App\Livewire\Admin\User\Users;
 use Illuminate\Support\Facades\Route;
 
@@ -36,6 +44,8 @@ Route::middleware([
 
     Route::prefix('room')->group(function () {
         Route::get('/', ShowRooms::class)->name('room.index');
+        Route::get('/create', CreateRoom::class)->name('room.create');
+        Route::get('/edit/{id}', EditRoom::class)->name('room.edit');
     });
 
     Route::prefix('room')->group(function () {
@@ -44,6 +54,19 @@ Route::middleware([
 
     Route::prefix('users')->group(function () {
         Route::get('/', Users::class)->name('users.index');
+        Route::get('/create', CreateUser::class)->name('user.create');
+        Route::get('/edit/{id}', EditUser::class)->name('user.edit');
+    });
+
+    Route::prefix('service')->group(function() {
+        Route::prefix('xtras')->group(function() {
+            Route::get('/', Xtras::class)->name('xtras.index');
+            Route::get('/create', CreateXtra::class)->name('xtra.create');
+        });
+        Route::prefix('tour')->group(function() {
+            Route::get('/', Tours::class)->name('tours.index');
+            Route::get('/create', CreateTour::class)->name('tour.create');
+        });
     });
     
 });
