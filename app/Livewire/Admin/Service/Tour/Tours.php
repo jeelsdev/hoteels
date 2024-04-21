c5695453acf851bcd00b4a2e66561c13560b582a
Joel Huaman <joelhuaman7m@gmail.com>
2024-04-21 10:37:40 -0500

    [FEAT] Add components for creating and listing tours

---
+++ app/Livewire/Admin/Service/Tour/Tours.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Service\Tour;
+
+use Livewire\Component;
+
+class Tours extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.service.tour.tours');
+    }
+}
