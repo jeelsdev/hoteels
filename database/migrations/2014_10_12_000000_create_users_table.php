4db5c73551d8bcf1c53bf72bb709c1bec1611f83
Joel Huaman <joelhuaman7m@gmail.com>
2024-05-05 13:01:39 -0500

    [FEAT] Update users and reservation tables with nullable fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('surname');
+            $table->string('surname')->nullable();
             $table->string('document_type');
             $table->string('document');
-            $table->string('phone');
-            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('favorite')->default(false)->nullable();
