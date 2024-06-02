74908d816fe7f28b86099be6d82050292f29b2d9
Joel Huaman <joelhuaman7m@gmail.com>
2024-06-02 16:43:26 -0500

    [FEAT] Add floor selection in CreateRoom component

--- app/Livewire/Admin/Room/ShowRooms.php
+++ app/Livewire/Admin/Room/ShowRooms.php
@@ -3,7 +3,10 @@
 namespace App\Livewire\Admin\Room;
 
 use App\Models\Room;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Livewire\Attributes\On;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class ShowRooms extends Component
@@ -21,10 +24,10 @@ class ShowRooms extends Component
         $rooms = Room::where('code', 'like', '%'.$this->search.'%')
             ->whereHas('roomType', function ($query) {
                 if ($this->roomType){
-                    $query->where('description', $this->roomType);
+                    $query->where('denomination', $this->roomType);
                 }
             })->when($this->floor, function ($query) {
-                $query->order('floor', $this->floor);
+                $query->orderBy('floor', $this->floor);
             })->paginate(20);
 
         $this->roomTypes = getEnumValues('RoomType');
