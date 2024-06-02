cc73777628f3b2be41db0d303704854aa65751b5
Joel Huaman <joelhuaman7m@gmail.com>
2024-06-02 17:20:38 -0500

    [FEAT] Update button component styling to use new background color and hover effect

--- resources/views/components/apps/button.blade.php
+++ resources/views/components/apps/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type'=> 'submit', 'class'=>'py-2 px-5 w-full text-base font-medium text-center text-white bg-vulcan-600 from-pink-500 to-vulcan-500 hover:scale-[1.02] shadow-md shadow-gray-300 transition-transform rounded-lg sm:w-auto flex justify-center items-center']) }}>
+<button {{ $attributes->merge(['type'=> 'submit', 'class'=>'py-2 px-5 w-full text-base font-medium text-center text-white bg-elf-green-600 from-pink-500 to-elf-green-500 hover:scale-[1.02] shadow-md shadow-gray-300 transition-transform rounded-lg sm:w-auto flex justify-center items-center hover:bg-elf-green-500']) }}>
     {{ $slot }}
 </button>
