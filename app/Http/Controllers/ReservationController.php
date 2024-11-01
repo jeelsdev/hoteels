5a01ccd03112e2c6fb8617e7c46f1ed09e4bbcbe
Jeels <joelhuaman7m@gmail.com>
2024-11-01 04:07:10 -0500

    [DEV] Implement reservation creation functionality and view

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Reservation;
 use App\Models\Room;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -17,5 +18,19 @@ class ReservationController extends Controller
         $reservations = Reservation::all();
 
         return view('admin.reservation.index', compact('rooms', 'reservations'));
+    }
+
+    public function create($data): View
+    {
+        parse_str($data, $data);
+
+        try {
+            $date = Carbon::parse($data['date']);
+            $room = Room::findOrFail($data['resource']);
+        } catch (\Throwable $th) {
+            return redirect()->route('reservation.index');
+        }
+
+        return view('admin.reservation.create', compact('date', 'room'));
     }
 }
